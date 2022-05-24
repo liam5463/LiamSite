e0b3cfa5e3393f3382f4685ba0a6fbf62f2d2dfa
Liam <liam_5463@live.com>
2022-05-24 12:14:21 +0100

    working quotes API

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
 	$(".clickHere").click(function (e) { 
 		e.preventDefault();
 		$(".bodyClass").toggleClass("bodyParty");
+
+		$.get("https://api.quotable.io/random", function(data){
+			$(".textGoesHere").text(data.content);
+		});
+
 	});
 	
 });
