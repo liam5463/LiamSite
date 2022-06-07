ace5b807d6b733848a1a64518ae381f5384b9427
Liam <liam_5463@live.com>
2022-06-07 14:21:24 +0100

    .

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
 	$(".clickHere").click(function (e) { 
 		e.preventDefault();
 		$(".bodyClass").toggleClass("bodyParty");
+	});
 
+	$(".getAQuote").click(function (e) {
+		e.preventDefault();
 		$.get("https://api.quotable.io/random", function(data){
 			$(".textGoesHere").text(data.content);
 		});
-
-	});
+	})
 	
 });
